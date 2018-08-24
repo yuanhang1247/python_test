78bb2e3b8410b4f53be39d80d0262ba75376d107
root <root@localhost.localdomain>
2018-08-24 16:02:49 +0800

    test4

--- day1.py
+++ day1.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 #
 #
-print("test")
+# -*- coding: utf-8 -*-
+import hashlib, random
+
+def get_md5(s):
+    return hashlib.md5(s.encode('utf-8')).hexdigest()
+
+class User(object):
+    def __init__(self, username, password):
+        self.username = username
+        self.salt = ''.join([chr(random.randint(48, 122)) for i in range(20)])
+        self.password = get_md5(password + self.salt)
+db = {
+    'michael': User('michael', '123456'),
+    'bob': User('bob', 'abc999'),
+    'alice': User('alice', 'alice2008')
+}
+
+def login(username, password):
+    user = db[username]
+    return user.password == get_md5(password)
